f07d22fd74b229b0b81ce8b9cf6d345a468a0099
Phelipe Soares Simeao <pssimeao@stefanini.com>
2019-08-12 16:00:01 -0300

    aula 4 - header

---
+++ huntweb/src/components/header/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => (
+    <header id="main-header">JSHunt</header>
+);
+
+
+//utilizar quando tiver um conceito de estado
+//igual a
+// class header extends component {
+//     render(){
+//         return <h1>Hello</h1>
+//     }
+//}
+
+export default Header;
